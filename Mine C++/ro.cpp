de299af3bc5e52559a1f086ab24caa76386f2a60
MrBT007 <bhuttushar2019@gmail.com>
2022-10-01 17:50:31 +0530

    1st oct

--- Mine C++/ro.cpp
+++ Mine C++/ro.cpp
@@ -17,24 +17,30 @@
 #define ppb pop_back
 #define fi first
 #define se second
-#define no cout<<"NO"<<endl;
-#define yes cout<<"YES"<<endl;
+#define no cout << "NO" << endl;
+#define yes cout << "YES" << endl;
 #define mod 1000000007
 #define all(x) (x).begin(), (x).end()
 #define SORT(v) sort(all(v))
-#define REVSORT(v) sort(all(v),greater<int>())
+#define REVSORT(v) sort(all(v), greater<int>())
 #define MAX(v) max_element(all(v))
 #define MIN(v) min_element(all(v))
-#define rep(from,to) for(int i = from;i<=to;i++)
-#define rep_back(from,to) for(int i = from;i>=to;i--)
-#define take(v) rep(0,v.size())cin>>v[i];
-#define FASTIO ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define rep(from, to) for (int i = from; i <= to; i++)
+#define rep_back(from, to) for (int i = from; i >= to; i--)
+#define take(v) rep(0, v.size()) cin >> v[i];
+#define FASTIO                        \
+    ios_base::sync_with_stdio(false); \
+    cin.tie(NULL);                    \
+    cout.tie(NULL)
 using namespace std;
 
 /* ========== TEMPLATES ========= */
 
-template<class A> istream& operator>>(istream& in, vector<A> &a){
-    for(A &i:a)in>>i;
+template <class A>
+istream &operator>>(istream &in, vector<A> &a)
+{
+    for (A &i : a)
+        in >> i;
     return in;
 }
 
@@ -49,19 +55,23 @@ ostream &operator<<(ostream &os, const T_container &v)
 
 bool isPowerOfTwo(int n)
 {
-    if(n==0)
-    return false;
+    if (n == 0)
+        return false;
     return (ceil(log2(n)) == floor(log2(n)));
 }
 
-bool is_prime(int n) {
-    if (n == 1) {
+bool is_prime(int n)
+{
+    if (n == 1)
+    {
         return false;
-}
+    }
 
     int i = 2;
-    while (i*i <= n) {
-        if (n % i == 0) {
+    while (i * i <= n)
+    {
+        if (n % i == 0)
+        {
             return false;
         }
         i += 1;
@@ -75,18 +85,27 @@ void google(int i)
 
 /* ========== YOUR CODE HERE ========= */
 
-// WUB* WE WUB+ ARE WUB WUB THE WUB CHAMPIONS WUB MY WUB FRIEND WUB*
+/*
+      0
+    0 1 0
+  0 1 2 1 0
+0 1 2 3 2 1 0
+  0 1 2 1 0
+    0 1 0
+      0
+*/
 
 int main()
 {
     FASTIO;
-    string s;cin>>s;
-    string ans = "";
-    for(int i = 0;i<s.length();i++)
+    int n;cin>>n;
+    for(int i = -n;i<=n;++i)
     {
-        
+        for(int j = 0;j<abs(i);++j)cout<<"  ";
+        for(int j = 0;j<n-abs(i);++j)cout<<j<<" ";
+        for(int j = n-abs(i);j>0;--j)cout<<j<<" ";
+        cout<<0<<"\n";
     }
-
 
     return 0;
 }
